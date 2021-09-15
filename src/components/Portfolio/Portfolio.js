f4b832fb6469df0d577ac1ec3fb17b1ffca0e6ea
Irakliy Dochviri <irakliy.d@yandex.ru>
2021-09-15 23:00:48 +0300

    feat: сверстать Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Portfolio.css";
+import { Link } from "react-router-dom";
 
 export default function Portfolio() {
-    return (
-        <div />
-    )
+  return (
+    <div className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <Link className="portfolio__link">Статичный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link className="portfolio__link">Адаптивный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link className="portfolio__link">Одностраничное приложение</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
