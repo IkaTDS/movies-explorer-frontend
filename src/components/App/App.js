09bf3845d180369c18e4ee99bfa21c957b1ac30e
Irakliy Dochviri <irakliy.d@yandex.ru>
2021-09-19 01:05:33 +0300

    feat: сверстать Profile, убрать Footer у / , /movies и /saved-movis;

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
-import { Route, Switch } from 'react-router-dom'
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import Profile from '../Profile/Profile';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import './App.css';
-import Header from '../Header/Header';
-import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import Profile from "../Profile/Profile";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import "./App.css";
+import Header from "../Header/Header";
+import React from "react";
 
 function App() {
   const [isBurgerMenuOpen, setBurgerMenuOpen] = React.useState(false);
-  
+
   function handleBurgerButtonClick() {
     setBurgerMenuOpen(true);
   }
@@ -23,38 +23,43 @@ function App() {
 
   return (
     <div className="page">
+      <Header
+        isBurgerMenuOpen={isBurgerMenuOpen}
+        burgerMenuOpen={handleBurgerButtonClick}
+        burgerMenuClose={handleBurgerCloseButtonClick}
+      />
 
-      <Header isBurgerMenuOpen={isBurgerMenuOpen} burgerMenuOpen={handleBurgerButtonClick} burgerMenuClose={handleBurgerCloseButtonClick}/>
-      
       <Switch>
-
-        <Route exact path='/'>
-          < Main />
+        <Route exact path="/">
+          <Main />
         </Route>
 
-        <Route path='/movies'>
-          < Movies />
+        <Route path="/movies">
+          <Movies />
         </Route>
 
-        <Route path='/saved-movies'>
-        < SavedMovies />
+        <Route path="/saved-movies">
+          <SavedMovies />
         </Route>
 
-        <Route path='/profile'>
-          < Profile />
+        <Route path="/profile">
+          <Profile />
         </Route>
 
-        <Route path='/signup'>
-          < Register />
+        <Route path="/signup">
+          <Register />
         </Route>
 
-        <Route path='/signin'>
-          < Login />
+        <Route path="/signin">
+          <Login />
         </Route>
-
       </Switch>
 
-      <Footer />
+      <Switch>
+        <Route exact path={["/", "/movies", "/saved-movies"]}>
+          <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
